6053d96f22f9353a0f8af146606136ebf43cb008
kelee <kelee9632@uniwiz.co.kr>
2025-07-15 08:54:30 +0900

    게임 상태를 위한 data class 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,7 @@ dependencies {
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    //viewmodel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
 }
---
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.unscramble.ui
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+
+}
